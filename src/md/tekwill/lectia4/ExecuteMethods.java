c1186bd7868f1f6961e4d26fd258a512be039fb0
anamunteant <anamuntean.me@gmail.com>
2022-04-03 22:39:47 +0300

    am adaugat cod pentru lectia 9

---
+++ src/md/tekwill/lectia4/ExecuteMethods.java
@@ -0,0 +1,16 @@
+package md.tekwill.lectia4;
+
+public class ExecuteMethods {
+    public static void main(String[] args) {
+        int rezultat = Methods.sum(30, 20);
+        System.out.println(rezultat);
+
+        int rezultat2 = Methods.sum(30, 30);
+        System.out.println(rezultat2);
+
+        int rezultat3 = Methods.sum(10, 100);
+        System.out.println(rezultat3);
+
+        Methods.sum(9);
+    }
+}
